aef45fce5b29508b938afe54fed469eae78e5154
Andrey <bolduzev@gmail.com>
2020-12-08 00:01:45 -0500

    Init day 8

---
+++ day08/src/lib.rs
@@ -0,0 +1,20 @@
+use aoc2020::parse;
+
+use std::path::Path;
+use thiserror::Error;
+use itertools::Itertools as _;
+use parse_display::{Display, FromStr};
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    unimplemented!()
+}
+
+pub fn part2(_input: &Path) -> Result<(), Error> {
+    unimplemented!()
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
